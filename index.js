7d369825621ffcce7c879a587d221f7907b6c4c3
Kshitij Katoch <kshitijkatoch213@gmail.com>
2025-08-08 18:22:21 +0530

    Routes: Added Products routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { initializeDatabase } = require("./db/db.connect");
 const Category = require("./models/category.models");
+const Product = require("./models/product.models");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -12,6 +13,59 @@ app.use(express.json());
 // DB Connection
 initializeDatabase();
 
+// ---- Products Routes ---- //
+// GET all products
+app.get("/api/products", async (req, res) => {
+  try {
+    const data = await Product.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch the data." });
+  }
+});
+
+// POST products
+app.post("/api/products", async (req, res) => {
+  try {
+    // const data = await new Product(req.body).save();
+    const body = Array.isArray(req.body) ? req.body : [req.body];
+    const data = await Product.insertMany(body);
+    res
+      .status(201)
+      .json({ message: "Product added successfully", product: data });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to add data" });
+  }
+});
+
+// GET product by ID
+app.get("/api/products/:productId", async (req, res) => {
+  try {
+    const data = await Product.findById(req.params.productId);
+    data
+      ? res.json(data)
+      : res.status(404).json({ error: "Product not found" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch the data." });
+  }
+});
+
+// DELETE product by id
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const data = await Product.findByIdAndDelete(req.params.id);
+    data
+      ? res.status(200).json({
+          message: "Product deleted successfully",
+          deleted: data,
+        })
+      : res.status(404).json({ error: "Product not found" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete data." });
+  }
+});
+
+// ---- Categories Routes ---- //
 // GET all categories
 app.get("/api/categories", async (req, res) => {
   try {
@@ -26,7 +80,8 @@ app.get("/api/categories", async (req, res) => {
 app.post("/api/categories", async (req, res) => {
   try {
     // const newCategory = await new Category(req.body).save();
-    const newCategory = await Category.insertMany(req.body);
+    const body = Array.isArray(req.body) ? req.body : [req.body];
+    const newCategory = await Category.insertMany(body);
     res
       .status(201)
       .json({ message: "Category added successfully", category: newCategory });
---
+++ models/product.models.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    brand: String,
+    name: String,
+    titleExtension: String,
+    description: String,
+    category: String,
+    size: [String],
+    price: Number,
+    image: String,
+    rating: Number,
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
